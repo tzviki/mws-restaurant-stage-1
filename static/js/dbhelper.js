18357f3891a7b8c9de29134fca81cce3d823e135
Tzviki Wolvovsky <tzviki@rallybound.com>
2018-09-16 21:41:07 -0400

    Updated dbhelper to use fetch

--- static/js/dbhelper.js
+++ static/js/dbhelper.js
@@ -16,18 +16,15 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const restaurants = JSON.parse(xhr.responseText);
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    fetch(DBHelper.DATABASE_URL, {method: 'GET'})
+      .then(response => {
+        response.json().then(restaurants => {
+          callback(null, restaurants);
+        });
+      })
+      .catch(error => {
+        callback(`fetch fail: ${error}`, null);
+      });
   }
 
   /**

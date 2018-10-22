2a8fbed5ecc1d6f3daf129aa64b72335c4a6484a
Tzviki Wolvovsky <tzviki@rallybound.com>
2018-10-22 14:45:52 -0400

    Fixed date on review

--- static/js/restaurant_info.js
+++ static/js/restaurant_info.js
@@ -174,7 +174,7 @@ createReviewHTML = (review) => {
   li.appendChild(name);
 
   const date = document.createElement('p');
-  date.innerHTML = review.date;
+  date.innerHTML = new Date(review.createdAt).toLocaleDateString('en-US');
   li.appendChild(date);
 
   const rating = document.createElement('p');
